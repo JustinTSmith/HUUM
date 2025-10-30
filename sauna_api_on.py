142cef5c862f3b7da8298ae35e862590a1e27242
JustinTSmith <justintsmith@gmail.com>
2025-10-30 16:06:26 -0700

    Update sauna_api_on.py

--- sauna_api_on.py
+++ sauna_api_on.py
@@ -4,8 +4,8 @@ import requests
 url = 'https://api.huum.eu/action/home/start'
 myobj = {'targetTemperature' : 40}
 #enter your username and password from app
-usern = 'yourusername@something.bla'
-passw = 'thepassword of your app'
+usern = 'justintsmith@gmail.com'
+passw = 'hy6Dr.Jm8391'
 x = requests.post(url, data = myobj, auth=(usern,passw))
 print(x.json())
 

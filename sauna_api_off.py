16c06feca64eaac51a0154cf2afb2830093c1cc8
JustinTSmith <justintsmith@gmail.com>
2025-10-30 16:07:16 -0700

    Update sauna_api_off.py

--- sauna_api_off.py
+++ sauna_api_off.py
@@ -2,8 +2,8 @@ import requests
 
 url = 'https://api.huum.eu/action/home/stop'
 #enter your username and password from app
-usern = 'yourusername@something.bla'
-passw = 'thepassword of your app'
+usern = 'justintsmith@gmail.com'
+passw = 'hy6Dr.Jm8391'
 x = requests.post(url, auth=(usern,passw))
 print(x.json())
 

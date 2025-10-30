b9dd0c3bb2daa0d6bc101423f6c2d4949fe4497d
JustinTSmith <justintsmith@gmail.com>
2025-10-30 16:07:41 -0700

    Update sauna_api_status.py

--- sauna_api_status.py
+++ sauna_api_status.py
@@ -2,8 +2,8 @@ import requests
 
 url = 'https://api.huum.eu/action/home/status'
 #enter your username and password from app
-usern = 'yourusername@something.bla'
-passw = 'thepassword of your app'
+usern = 'justintsmith@gmail.com'
+passw = 'hy6Dr.Jm8391'
 x = requests.get(url, auth=(usern,passw))
 print(x.json())
 
